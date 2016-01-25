86e38ee9a1d989d9ba145d5e5a29880275b19a1b
gkobilansky <genek149@gmail.com>
2016-01-25 14:12:58 -0500

    height fix

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ $(document).ready(function () {
             //            };
 
             width = $(window).innerWidth();
-            height = $(window).innerHeight();
+            height = $(window).innerHeight() - 50;
             target = {
                 x: width / 2,
                 y: height / 2
